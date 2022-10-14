170f14250c3bcc007dd2390dc6a401e2b830532e
Niton151 <niton984@gmail.com>
2022-10-14 23:27:30 +0900

    ノーツの出現位置と判定

--- Assets/Scripts/Rhythm/NotesController.cs
+++ Assets/Scripts/Rhythm/NotesController.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Threading;
 using Cysharp.Threading.Tasks;
 using ObjectPool;
+using UniRx;
+using UniRx.Triggers;
 using UnityEngine;
 using UnityEngine.VFX;
 
@@ -8,26 +11,59 @@ namespace Rhythm
 {
     public class NotesController : MonoBehaviour
     {
-        [SerializeField] private float closeTime;
-        [SerializeField] private float perfectRange;
-        [SerializeField] private float greatRange;
-        [SerializeField] private float goodRange;
-        [SerializeField] private float badRange;
+        [SerializeField, Tooltip("リングが閉じるまでの時間")]
+        private float closeTime;
 
-        private float _lifeTime;
+        [SerializeField, Tooltip("Perfectが出る範囲")]
+        private float perfectRange;
+
+        [SerializeField, Tooltip("Greatが出る範囲")]
+        private float greatRange;
+
+        [SerializeField, Tooltip("Goodが出る範囲")] private float goodRange;
+        [SerializeField, Tooltip("判定が出る範囲")] private float badRange;
+
+        private float _lifeTime = 0f; // 生成されてからの時間
         private VisualEffect _vfx;
+        private VFXObjectPool _pool;
+        private Collider _collider;
+        private CancellationTokenSource _cts;
 
-        public async void Initialize(VFXObjectPool pool)
+        /// <summary>
+        /// 現在判定中のノーツ
+        /// </summary>
+        public static int NowNoteNum = 0;
+
+        // 出現
+        public async UniTaskVoid Initialize(VFXObjectPool pool, int beatCount)
         {
+            _cts = new CancellationTokenSource();
+            _pool = pool;
             _vfx = GetComponent<VisualEffect>();
             _vfx.SetFloat("CloseTime", closeTime);
+            _collider = GetComponent<Collider>();
+            _lifeTime = 0;
 
-            await UniTask.Delay(TimeSpan.FromSeconds(closeTime),
-                cancellationToken: this.GetCancellationTokenOnDestroy());
-            
-            NotesGenerator.IsAudioPlay = true;
+            // 前のノーツが消えるまで待つ
+            await UniTask.WaitUntil(() => NowNoteNum == beatCount, cancellationToken: _cts.Token);
 
-            pool.Return(this);
+            _collider.enabled = true;
+            this.OnTriggerEnterAsObservable()
+                .Where(x => x.CompareTag("Hand"))
+                .Subscribe(_ => Hit()).AddTo(_cts.Token);
+
+            if (beatCount == 0)
+            {
+                await UniTask.WaitUntil(() => _lifeTime >= closeTime, cancellationToken: _cts.Token);
+
+                NotesGenerator.IsAudioPlay = true;
+                Finish();
+            }
+
+            await UniTask.WaitUntil(() => _lifeTime >= closeTime + badRange, cancellationToken: _cts.Token);
+
+            // 時間切れ 
+            Finish();
         }
 
         private void FixedUpdate()
@@ -35,25 +71,46 @@ namespace Rhythm
             _lifeTime += Time.deltaTime;
         }
 
+        /// <summary>
+        /// パンチがノーツに当たった時の処理
+        /// </summary>
         private void Hit()
         {
             var diff = Math.Abs(_lifeTime - closeTime);
             if (diff <= perfectRange)
             {
-                Debug.Log("Perfect");
+                Debug.Log("Perfect" + NowNoteNum);
             }
             else if (diff <= greatRange)
             {
-                Debug.Log("Great");
+                Debug.Log("Great" + NowNoteNum);
             }
             else if (diff <= goodRange)
             {
-                Debug.Log("Good");
+                Debug.Log("Good" + NowNoteNum);
             }
-            else if(diff <= badRange)
+            else
             {
-                Debug.Log("Bad");
+                Debug.Log("Bad" + NowNoteNum);
             }
+
+            Finish();
+        }
+
+        /// <summary>
+        /// ノーツの消滅時の処理
+        /// </summary>
+        private void Finish()
+        {
+            _pool.Return(this);
+            NowNoteNum++;
+            _collider.enabled = false;
+            if (!_cts.IsCancellationRequested)
+            {
+                _cts.Cancel();
+            }
+
+            _cts.Dispose();
         }
     }
 }
--- Assets/Scripts/Rhythm/NotesGenerator.cs
+++ Assets/Scripts/Rhythm/NotesGenerator.cs
@@ -1,9 +1,12 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Cysharp.Threading.Tasks;
 using ObjectPool;
 using UniRx;
 using UniRx.Triggers;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Rhythm
 {
@@ -24,18 +27,44 @@ namespace Rhythm
             public int block;
             public int LPB;
         }
+        
+        [SerializeField, Tooltip("子オブジェクトがスポナー")] private GameObject spawnPosParent;
+        private List<Transform> _spawnPos;
+        private Transform _prePos; // 重複防止用
 
-        private int[] _scoreNum;
+        /// <summary>
+        /// (ノーツがある)譜面の位置番号
+        /// </summary>
+        private int[] _scoreNum;   
+        
+        /// <summary>
+        /// (ノーツがある)譜面のレーン番号
+        /// </summary>
         private int[] _scoreBlock;
+        
         private int _BPM;
+        
+        /// <summary>
+        /// 1拍当たりの分割数
+        /// </summary>
         private int _LPB;
+        
         private float _offset;
 
         private float _nowTime;
-        private float _moveSpan = 0.02f;
+        private float _moveSpan = 0.02f;    // FixedUpdateの間隔
+        
+        /// <summary>
+        /// 生成したノーツ数
+        /// </summary>
         private int _beatCount;
+        
+        /// <summary>
+        /// 現在の再生位置
+        /// </summary>
         private int _beatNum;
-        private bool _isBeat;
+        
+        private bool _isBeat;   // かぶり防止用
 
         private AudioSource _audioSource;
         private VFXObjectPoolProvider _vfxProvider;
@@ -48,10 +77,14 @@ namespace Rhythm
             _audioSource = GetComponent<AudioSource>();
             _vfxProvider = GetComponent<VFXObjectPoolProvider>();
 
+            _spawnPos = spawnPosParent.GetComponentsInChildren<Transform>().ToList();
+
+            // オブジェクトプールを生成
             _vfxPool = _vfxProvider.Get();
             
             ReadMusic();
 
+            // スタート条件
             await UniTask.WaitUntil(() => Input.GetKeyDown(KeyCode.Space),
                 cancellationToken: this.GetCancellationTokenOnDestroy());
             
@@ -59,20 +92,25 @@ namespace Rhythm
                 .Subscribe(_ => GenerateNotes())
                 .AddTo(this);
 
+            // 最初の空ノーツが判定ラインに来たら音楽を鳴らす
             await UniTask.WaitUntil(() => IsAudioPlay, cancellationToken: this.GetCancellationTokenOnDestroy());
             _audioSource.Play();
         }
 
+        /// <summary>
+        /// Jsonファイルを読み込む
+        /// </summary>
         private void ReadMusic()
         {
             string inputString = Resources.Load<TextAsset>("Noesis").ToString();
             InputJson inputJson = JsonUtility.FromJson<InputJson>(inputString);
 
+            // 値を各変数に代入
             _scoreNum = new int[inputJson.notes.Length];
             _scoreBlock = new int[inputJson.notes.Length];
             _BPM = inputJson.BPM;
             _LPB = inputJson.notes[0].LPB;
-            _offset = inputJson.offset * 0.0001f;
+            _offset = inputJson.offset * 0.0001f;   // Offsetの単位がわからないから大体の値
 
             for (int i = 0; i < inputJson.notes.Length; i++)
             {
@@ -104,12 +142,30 @@ namespace Rhythm
                 if (_scoreBlock[_beatCount] == 0)
                 {
                     var note = _vfxPool.Rent();
-                    note.Initialize(_vfxPool);
+                    note.transform.position = GetRandomPosition();
+                    note.Initialize(_vfxPool, _beatCount).Forget();
+                }
+                else
+                {
+                    NotesController.NowNoteNum++;
                 }
 
                 _beatCount++;
                 _isBeat = false;
             }
+        }
+
+        private Vector3 GetRandomPosition()
+        {
+            if (_prePos != null)
+            {
+                _spawnPos.Add(_prePos);
+            }
+            var rand = Random.Range(0, _spawnPos.Count);
+            _prePos = _spawnPos[rand];
+            _spawnPos.RemoveAt(rand);
+            
+            return _prePos.position;
         }
     }
 }
